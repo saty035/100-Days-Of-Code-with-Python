8df21137f82e1f1a3e190f3743b3d95fafcde072
saty035 <71729770+saty035@users.noreply.github.com>
2020-10-21 23:29:02 +0530

    +Day 4  paddle movement fixed

--- Day 3/PingPong.py
+++ Day 3/PingPong.py
@@ -131,7 +131,7 @@ while True:
     if (ball.xcor()<-340 and ball.xcor()>-350) and ball.ycor()<paddle_a.ycor()+40 and ball.ycor()>paddle_a.ycor()-40:
         ball.setx(-340)
         ball.dx *= -1
-  
+ 
 
 
 
